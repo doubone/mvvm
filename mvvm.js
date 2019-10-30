e875e78550a14f833b50a05251381090726ac1fa
licheng <18892080039@163.com>
2019-10-30 22:59:01 +0800

    把元素节点放到内存中

--- mvvm.js
+++ mvvm.js
@@ -6,17 +6,21 @@ class Compliler{
     constructor(el,vm){
         //判断el是不是元素，如果不是元素就获取他
         this.el = this.isElementNode(el)?el:document.querySelector(el);
-        console.log(this.el)
         let fragment = this.node2fragment(this.el);
     }
     node2fragment(node){
         //创建一个文档碎片
         let fragment = document.createDocumentFragment();
+        debugger
         let firstChild;
         while(firstChild = node.firstChild){
             console.log(firstChild)
             fragment.appendChild(firstChild);
         }
+        // console.log(node.children)
+        // node.child.forEach((e)=>{
+        //     fragment.appendChild(e)
+        // })
         return fragment;
     }
     isElementNode(node){//是不是元素节点
