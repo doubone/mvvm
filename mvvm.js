38cd334d2e213af81701ec85d8c0e8bb54933115
licheng <18892080039@163.com>
2019-11-04 07:30:44 +0800

    编译过程

--- mvvm.js
+++ mvvm.js
@@ -6,25 +6,98 @@ class Compliler{
     constructor(el,vm){
         //判断el是不是元素，如果不是元素就获取他
         this.el = this.isElementNode(el)?el:document.querySelector(el);
+
+        this.vm = vm;
         let fragment = this.node2fragment(this.el);
+        //把节点内容替换
+        //编译模板 用数据编译
+        this.compile(fragment);
+        //把内容填充进页面
+        this.el.appendChild(fragment)
     }
+    isDirective(attrName){
+        return attrName.startsWith("v-")
+    }
+    //编译元素
+    compileElement(node){
+        const attributes = node.attributes; //类数组
+        [...attributes].forEach(attr=>{
+            let {name , value:expr} =attr ; //v-model="school.name"
+                if(this.isDirective(name)){
+                    console.log(node,"element")
+                    let [,directive] = name.split("-");
+                    // 需要调用不同的指令来处理
+                    CompileUtils[directive](node,expr,this.vm)
+                }
+        })
+    }
+    //编译文本
+    compileText(node){ //判断当前文本中是否包含大括号 {{aaa}}
+        let content = node.textContent;
+        if(/\{\{(.+?)\}\}/.test(content)){
+            console.log(content,"text")
+            CompileUtils['text'](node,content,this.vm);
+        }
+    }
+    compile(node){//用来编译内存中的dom节点
+        let childNodes = node.childNodes;
+        [...childNodes].forEach(child=>{
+            if(this.isElementNode(child)){
+                this.compileElement(child)
+                //如果是元素，需要把自己传进去 去遍历子节点
+                this.compile(child)
+            }else{
+                this.compileText(child)
+            }
+        })
+    }
+    //把节点移动到内存中
     node2fragment(node){
         //创建一个文档碎片
         let fragment = document.createDocumentFragment();
-        debugger
         let firstChild;
         while(firstChild = node.firstChild){
-            console.log(firstChild)
             fragment.appendChild(firstChild);
         }
-        // console.log(node.children)
-        // node.child.forEach((e)=>{
-        //     fragment.appendChild(e)
-        // })
         return fragment;
     }
     isElementNode(node){//是不是元素节点
        return  node.nodeType === 1 ;
+    }
+}
+
+CompileUtils ={
+    //根据表达式取到对应的数据
+    getValue(vm,expr){ //vm.$data school.name
+       return  expr.split(".").reduce((data,current)=>{
+                return data[current]
+        },vm.$data)
+
+    },
+    model(node,expr,vm){ //node是节点  expr是表达式 vm是当前实例
+        let fn = this.updater['modelUpdater'];
+        let value =this.getValue(vm,expr);
+        fn(node,value)
+    },
+    html(){
+
+    },
+    text(node,expr,vm){//expr -> {{a}} {{b}}
+        let fn = this.updater['textUpdater']
+        let content = expr.replace(/\{\{(.+?)\}\}/g,(...args)=>{
+            return this.getValue(vm,args[1])
+        })
+        fn(node,content)
+    },
+    updater:{
+        //把数据插入到节点中
+        modelUpdater(node,value){
+            node.value =value;
+        },
+        //处理文本节点
+        textUpdater(node,value){
+            node.textContent = value ;
+        }
     }
 }
 
